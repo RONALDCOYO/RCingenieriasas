29a2bf9502f080f977d6c4d9c273caa287277fa4
Spaguetto <48161646+Spaguetto@users.noreply.github.com>
2024-01-24 19:49:26 -0500

    Se agrega la carpeta img para las imagenes se modifica el archivo index y servicios Me gustaria que en este Pull request revisaras estos cambios y te animaras a hacer este contador / impresion de los nuemeros del 1 al 10.

---
+++ Index.java
@@ -0,0 +1,10 @@
+// Hey  deberias hacer algo en este archivo
+// te propongo hacer una clase que imprima los numeros del al 10
+
+// ejemplo 
+
+public class Index {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
